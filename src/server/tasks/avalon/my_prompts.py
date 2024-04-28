6281afdf9b731c8cf2dca6932eb6bf97ee1c8542
KwanWaiChung <wckwan@kfsrv05.se.cuhk.edu.hk>
2024-04-28 23:33:03 +0800

    update the prompt

--- src/server/tasks/avalon/my_prompts.py
+++ src/server/tasks/avalon/my_prompts.py
@@ -20,15 +20,15 @@ CHOOSE_TEAM_ACTION = """Please choose {} players from player ids 0 to {} as team
 
 TEAM_VOTE = """Review the game history, your role, and previous discussions among players. Decide whether to vote `approve` or `reject` the Leader's proposed Quest team with Players {team}. Note that the vote is anonymous. Players will only know the number of `approve` and `reject` without knowing who vote them. Provide your rationale and your final answer as `approve` or `reject`. Format your output as JSON: 
 {
-    "rationale": "<str>",
-    "vote: "<str, either `approve` or `reject`."
+    "rationale": <str>,
+    "vote: <str, either `approve` or `reject`>
 }"""
 
 
 VOTE_MISSION_ACTION = """The team with players {team} was approved, which includes you. Review the game history, your role, and previous discussions among players. Decide whether to vote `pass` or `fail` for the current Quest. Note that the vote is anonymous. Players will only know the number of `pass` and `fail` without knowing who vote them. Provide your rationale and your final answer as `pass` of `fail`. Format your output as JSON: 
 {
-    "rationale": "<str>",
-    "vote: "<str, either `pass` or `fail`."
+    "rationale": <str>,
+    "vote: <str, either `pass` or `fail`>
 }"""
 
 ASSASSINATION_PROMPT = """Now is the assassination phase. Based on the provided game play history, which player do you think is Merlin? Provide your rationale and your final answer. The valid player ids are 0 to {max_player_id}. Format your output as JSON:
@@ -45,8 +45,8 @@ SUMMARIZE = "Please summarize the history. Try to keep all useful information, i
 TEAM_DISCUSSION = """You are going to make some statements during this discussion to gain an advantage for your side. First, provide me your strategy in 150 words in this discussion round, and explain how you intend to use them to gain an advantage for your team. Then, provide your response in 100 words directly without further explanation. Your response should include your suggestion for the team proposal. All players can read your response so be careful not to leak important information. Ground your response on the provided game play history and do not fabricate facts.
 Format your output as JSON:  
 {
-    "strategy": "",
-    "response: ""
+    "strategy": <str>,
+    "response: <str>
 }"""
 
 
@@ -54,7 +54,7 @@ PROPOSE_TEAM_PROMPT = """You are the Quest leader. Choose {num_player} players f
 Format your output as JSON:
 {
     "rationale": <str>,
-    "team": [<int>],
+    "team": [<int>]
 }
 where `team` is the list of player ids of the proposed team. For example, [0, 2, 3] denotes Players 0, 2, and 3 on the team."""
 
@@ -71,11 +71,11 @@ Format your output as JSON:
 {
     "Merlin: {
         "rationale": <str: rationale to support being Merlin>,
-        "score": <int: 1 to 10>,
+        "score": <int: 1 to 10>
     },
     "Servant": {
         "rationale": <str: rationale to support being Servant>,
-        "score": <int: 1 to 10>,
+        "score": <int: 1 to 10>
     }
 }"""
 
@@ -84,15 +84,15 @@ Format your output as JSON:
 {
     "Merlin": {
         "rationale": <str: rationale to support being Merlin>,
-        "score": <int: 1 to 10>,
+        "score": <int: 1 to 10>
     },
     "Servant": {
         "rationale": <str: rationale to support being Servant>,
-        "score": <int: 1 to 10>,
+        "score": <int: 1 to 10>
     },
     "Minion": {
         "rationale": <str: rationale to support being Minion>,
-        "score": <int: 1 to 10>,
+        "score": <int: 1 to 10>
     }
 }"""
 
