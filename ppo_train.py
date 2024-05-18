59b0cf1b497ed147c417b23072ac042c8ab3cb8c
KwanWaiChung <wckwan@kfsrv05.se.cuhk.edu.hk>
2024-05-18 22:30:54 +0800

    Add data processing code but not yet double checked.

--- ppo_train.py
+++ ppo_train.py
@@ -15,6 +15,21 @@ from strictfire import StrictFire
 from peft import PeftModel, LoraConfig, get_peft_model
 from fastchat.conversation import get_conv_template
 
+TRAJ_WIN_REWARD = 1
+TRAJ_LOSE_REWARD = -1
+ROLE_GUESS_SCORE = {
+    1: -5,
+    2: -4,
+    3: -3,
+    4: -2,
+    5: -1,
+    6: 1,
+    7: 2,
+    8: 3,
+    9: 4,
+    10: 5,
+}
+
 
 # get models
 def get_model(model_name: str, lora_path: str = None):
@@ -53,39 +68,230 @@ def get_tokenizer(model_name: str):
     return tokenizer
 
 
-def get_data(filename: str):
+def get_data(
+    filename: str, include_guess_role: str, include_guess_belief: str
+):
     data = [json.loads(row) for row in open(filename)]
-    seeder = random.Random(111)
     out_data = []
     for row in data:
-        for round in row["role_guess"]:
-            for item in round:
-                tgt_role: str = row["roles"][item["tgt_player"]][1]
-                role = seeder.choice(list(item["output"].keys()))
-                res = item["output"][role]
-                if role == tgt_role or (
-                    role == "Minion" and tgt_role == "Assassin"
-                ):
-                    # 5 for score=10, -5 for score=0
-                    reward = 5 - (10 - res["score"])
-                else:
-                    # -5 for score=10, 5 for score=0
-                    reward = -(5 - (10 - res["score"]))
-                prompt = (
-                    item["prompt"]
-                    .replace("Merlin and Servant", role)
-                    .replace("Merlin, Servant, and Minion", role)
+        good_win = row["final_result"]
+        player_rewards = {
+            i: TRAJ_WIN_REWARD if good_win == role[-1] else TRAJ_LOSE_REWARD
+            for i, role in enumerate(row["roles"])
+        }
+        roles = {i: role[1] for i, role in enumerate(row["roles"])}
+        # team disc
+        for disc_round in row["team_discs"]:
+            for player_i, disc_dict in disc_round.items():
+                # ["strategy", "response", "prompt"]
+                out_data.append(
+                    (
+                        disc_dict["prompt"],
+                        json.dumps(
+                            {
+                                "strategy": disc_dict["strategy"],
+                                "response": disc_dict["response"],
+                            },
+                            indent=4,
+                            ensure_ascii=False,
+                        ),
+                        player_rewards[int(player_i)],
+                    )
                 )
-                prompt = prompt.split("{")[0]
-                prompt += f"""{{
-    "{role}": {{
-        "rationale": <str: rationale to support being {role}>,
-        "score": <int: 1 to 10>
-    }}
-}}"""
-                output = {role: item["output"][role]}
-                response = json.dumps(output, ensure_ascii=False, indent=4)
-                out_data.append((prompt, response, reward))
+        # team props
+        for leader, prop_dict in zip(row["leaders"], row["team_props"]):
+            # ["rationale", "team", "prompt"]
+            out_data.append(
+                (
+                    prop_dict["prompt"],
+                    json.dumps(
+                        {
+                            "rationale": prop_dict["rationale"],
+                            "team": prop_dict["team"],
+                        },
+                        indent=4,
+                        ensure_ascii=False,
+                    ),
+                    player_rewards[int(leader)],
+                )
+            )
+
+        # team votes
+        for vote_round in row["team_votes"]:
+            for player_i, vote_dict in vote_round["votes"].items():
+                # ["rationale", "team", "prompt"]
+                out_data.append(
+                    (
+                        vote_dict["prompt"],
+                        json.dumps(
+                            {
+                                "rationale": vote_dict["rationale"],
+                                "vote": (
+                                    "approve"
+                                    if vote_dict["vote"]
+                                    else "reject"
+                                ),
+                            },
+                            indent=4,
+                            ensure_ascii=False,
+                        ),
+                        player_rewards[int(player_i)],
+                    )
+                )
+
+        # quest votes
+        for vote_round in row["quest_votes"]:
+            for player_i, vote_dict in vote_round["votes"].items():
+                # ["rationale", "team", "prompt"]
+                out_data.append(
+                    (
+                        vote_dict["prompt"],
+                        json.dumps(
+                            {
+                                "rationale": vote_dict["rationale"],
+                                "vote": (
+                                    "pass" if vote_dict["vote"] else "fail"
+                                ),
+                            },
+                            indent=4,
+                            ensure_ascii=False,
+                        ),
+                        player_rewards[int(player_i)],
+                    )
+                )
+
+        # role_guess
+        if include_guess_role:
+            for guess_round in row["role_guess"]:
+                for src_player_i, guess_dicts in guess_round["votes"].items():
+                    for guess_dict in guess_dicts:
+                        # [output, prompt, src_player:int, tgt_player:int]
+                        sign: bool = (
+                            guess_dict["tgt_role"]
+                            == roles[guess_dict["tgt_player"]]
+                        )
+                        out_data.append(
+                            (
+                                guess_dict["prompt"],
+                                json.dumps(
+                                    guess_dict["output"],
+                                    indent=4,
+                                    ensure_ascii=False,
+                                ),
+                                int(sign)
+                                * ROLE_GUESS_SCORE[guess_dict["score"]],
+                            )
+                        )
+
+        # role_belief
+        if include_guess_belief:
+            for round_i, belief_round in enumerate(row["role_belief"]):
+                for src_player_i, belief_dict in belief_round["votes"].items():
+                    # [output, prompt, src_player:int, tgt_player:int]
+                    tgt_player: int = belief_dict["tgt_player"]
+                    tgt_role: str = roles[tgt_player]
+                    src_role: str = roles[src_player_i]
+                    to_guess_role: str = belief_dict["tgt_role"]
+                    if src_role == "Servant" and tgt_role == "Merlin":
+                        if to_guess_role == "Servant":
+                            gold_score = 10
+                        else:
+                            gold_score = 1
+                    elif src_role in ["Minion", "Assassin"] and tgt_role in [
+                        "Minion",
+                        "Assassin",
+                        "Merlin",
+                    ]:
+                        if to_guess_role in ["Minion", "Assassin"]:
+                            gold_score = 10
+                        else:
+                            gold_score = 1
+                    else:
+                        guesses = []
+                        for role_guess in row["role_guess"][round_i][
+                            tgt_player
+                        ]:
+                            guesses.append(role_guess["tgt_player"])
+                            if role_guess["tgt_player"] == src_player_i:
+                                gold_score: int = role_guess["output"]["score"]
+                                break
+                        else:
+                            raise ValueError(
+                                f"`gold_score` not provided in role_guess. Player {tgt_player} only guesses Player {guesses}'s role, but not {src_player_i}."
+                            )
+
+                    sign: bool = (
+                        belief_dict["tgt_role"]
+                        == roles[belief_dict["tgt_player"]]
+                    )
+                    out_data.append(
+                        (
+                            belief_dict["prompt"],
+                            json.dumps(
+                                belief_dict["output"],
+                                indent=4,
+                                ensure_ascii=False,
+                            ),
+                            abs(belief_dict["score"] - gold_score),
+                        )
+                    )
+        # summaries
+        for sum_round in row["summaries"]:
+            for player_i, sum_dict in sum_round.items():
+                # ["prompt", "resp"]
+                out_data.append(
+                    (
+                        sum_dict["prompt"],
+                        sum_dict["resp"],
+                        player_rewards[int(player_i)],
+                    )
+                )
+
+        # assassin
+        if row["assassin"]:
+            out_data.append(
+                (
+                    row["assassin"]["prompt"],
+                    json.dumps(
+                        {
+                            "rationale": row["assassin"]["rationale"],
+                            "merlin": row["assassin"]["merlin"],
+                        },
+                        indent=4,
+                        ensure_ascii=False,
+                    ),
+                    TRAJ_LOSE_REWARD if good_win else TRAJ_WIN_REWARD,
+                )
+            )
+    #         # here is the old code to handle multiple role guess
+    #         for round in row["role_guess"]:
+    #             for item in round:
+    #                 tgt_role: str = row["roles"][item["tgt_player"]][1]
+    #                 role = seeder.choice(list(item["output"].keys()))
+    #                 res = item["output"][role]
+    #                 if role == tgt_role or (
+    #                     role == "Minion" and tgt_role == "Assassin"
+    #                 ):
+    #                     # 5 for score=10, -5 for score=0
+    #                     reward = 5 - (10 - res["score"])
+    #                 else:
+    #                     # -5 for score=10, 5 for score=0
+    #                     reward = -(5 - (10 - res["score"]))
+    #                 prompt = (
+    #                     item["prompt"]
+    #                     .replace("Merlin and Servant", role)
+    #                     .replace("Merlin, Servant, and Minion", role)
+    #                 )
+    #                 prompt = prompt.split("{")[0]
+    #                 prompt += f"""{{
+    #     "{role}": {{
+    #         "rationale": <str: rationale to support being {role}>,
+    #         "score": <int: 1 to 10>
+    #     }}
+    # }}"""
+    #                 output = {role: item["output"][role]}
+    #                 response = json.dumps(output, ensure_ascii=False, indent=4)
+    #                 out_data.append((prompt, response, reward))
     return out_data
 
 
@@ -128,12 +334,18 @@ def main(
     grad_accum: int = 32,
     n_epochs: int = 4,
     n_samples: int = -1,  # -1 means all.
+    include_guess_role: bool = False,
+    include_guess_belief: bool = False,
     save_path: str = "saves/avalon_ppo",
 ):
-    model, model_ref = get_model(model_name, lora_path=lora_path)
-    tokenizer = get_tokenizer(model_name)
+    # model, model_ref = get_model(model_name, lora_path=lora_path)
+    # # tokenizer = get_tokenizer(model_name)
 
-    dataset = get_data(filename)
+    dataset = get_data(
+        filename,
+        include_guess_role=include_guess_role,
+        include_guess_belief=include_guess_belief,
+    )
 
     prompt_tensors = []
     response_tensors = []
