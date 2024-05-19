cd9ce79a4f686ffa95d0e72ff2ec9b5ce6d76a56
KwanWaiChung <wckwan@kfsrv04.se.cuhk.edu.hk>
2024-05-19 23:12:30 +0800

    Add some ppo train code.
    Not sure if it works since there are some weird deepspeed bugs in kfsrv04.

--- ppo_train.py
+++ ppo_train.py
@@ -4,6 +4,7 @@ import random
 import json
 from accelerate import Accelerator
 from typing import Tuple, Dict, Any
+from torch.utils.data import Dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import (
     PPOConfig,
@@ -11,6 +12,7 @@ from trl import (
     create_reference_model,
     PPOTrainer,
 )
+from ppo_trainer import PPOTrainer
 from strictfire import StrictFire
 from peft import PeftModel, LoraConfig, get_peft_model
 from fastchat.conversation import get_conv_template
@@ -42,6 +44,278 @@ ROLE_BELIEF_SCORE = {
     8: -4,
     9: -5,
 }
+accelerator = Accelerator()
+
+is_deepspeed_used = accelerator.distributed_type == "DEEPSPEED" and hasattr(
+    accelerator.state, "deepspeed_plugin"
+)
+
+
+class PPODataset(Dataset):
+    def __init__(
+        self,
+        filename,
+        mini_batch_size: int,
+        grad_accum: int,
+        n_samples: int,
+        tokenizer,
+        include_guess_role: bool = False,
+        include_guess_belief: bool = False,
+    ):
+        data = [json.loads(row) for row in open(filename)]
+        data = [convert_int_keys(row) for row in data]
+        out_data = []
+        for row in data:
+            good_win = row["final_result"]
+            player_rewards = {
+                i: (
+                    TRAJ_WIN_REWARD
+                    if good_win == role[-1]
+                    else TRAJ_LOSE_REWARD
+                )
+                for i, role in enumerate(row["roles"])
+            }
+            roles = {i: role[1] for i, role in enumerate(row["roles"])}
+            # team disc
+            for disc_round in row["team_discs"]:
+                for player_i, disc_dict in disc_round.items():
+                    # ["strategy", "response", "prompt"]
+                    out_data.append(
+                        (
+                            disc_dict["prompt"],
+                            json.dumps(
+                                {
+                                    "strategy": disc_dict["strategy"],
+                                    "response": disc_dict["response"],
+                                },
+                                indent=4,
+                                ensure_ascii=False,
+                            ),
+                            player_rewards[int(player_i)],
+                        )
+                    )
+            # team props
+            for leader, prop_dict in zip(row["leaders"], row["team_props"]):
+                # ["rationale", "team", "prompt"]
+                out_data.append(
+                    (
+                        prop_dict["prompt"],
+                        json.dumps(
+                            {
+                                "rationale": prop_dict["rationale"],
+                                "team": prop_dict["team"],
+                            },
+                            indent=4,
+                            ensure_ascii=False,
+                        ),
+                        player_rewards[int(leader)],
+                    )
+                )
+
+            # team votes
+            for vote_round in row["team_votes"]:
+                for player_i, vote_dict in vote_round["votes"].items():
+                    # ["rationale", "team", "prompt"]
+                    out_data.append(
+                        (
+                            vote_dict["prompt"],
+                            json.dumps(
+                                {
+                                    "rationale": vote_dict["rationale"],
+                                    "vote": (
+                                        "approve"
+                                        if vote_dict["vote"]
+                                        else "reject"
+                                    ),
+                                },
+                                indent=4,
+                                ensure_ascii=False,
+                            ),
+                            player_rewards[int(player_i)],
+                        )
+                    )
+
+            # quest votes
+            for vote_round in row["quest_votes"]:
+                for player_i, vote_dict in vote_round["votes"].items():
+                    # ["rationale", "team", "prompt"]
+                    out_data.append(
+                        (
+                            vote_dict["prompt"],
+                            json.dumps(
+                                {
+                                    "rationale": vote_dict["rationale"],
+                                    "vote": (
+                                        "pass" if vote_dict["vote"] else "fail"
+                                    ),
+                                },
+                                indent=4,
+                                ensure_ascii=False,
+                            ),
+                            player_rewards[int(player_i)],
+                        )
+                    )
+
+            # role_guess
+            if include_guess_role:
+                for guess_round in row["role_guess"]:
+                    for src_player_i, guess_dicts in guess_round.items():
+                        tgt_players = set()
+                        for guess_dict in guess_dicts:
+                            # [output, prompt, src_player:int, tgt_player:int]
+                            if guess_dict["tgt_player"] not in tgt_players:
+                                # duplicate guesses exist in previous code
+                                sign: bool = (
+                                    guess_dict["tgt_role"]
+                                    == roles[guess_dict["tgt_player"]]
+                                )
+                                out_data.append(
+                                    (
+                                        guess_dict["prompt"],
+                                        json.dumps(
+                                            guess_dict["output"],
+                                            indent=4,
+                                            ensure_ascii=False,
+                                        ),
+                                        int(sign)
+                                        * ROLE_GUESS_SCORE[
+                                            guess_dict["output"]["score"]
+                                        ],
+                                    )
+                                )
+                                tgt_players.add(guess_dict["tgt_player"])
+
+            # role_belief
+            if include_guess_belief:
+                for round_i, belief_round in enumerate(row["role_belief"]):
+                    for src_player_i, belief_dict in belief_round.items():
+                        # [output, prompt, src_player:int, tgt_player:int]
+                        tgt_player: int = belief_dict["tgt_player"]
+                        tgt_role: str = roles[int(tgt_player)]
+                        src_role: str = roles[int(src_player_i)]
+                        to_guess_role: str = belief_dict["tgt_role"]
+                        if src_role == "Servant" and tgt_role == "Merlin":
+                            if to_guess_role == "Servant":
+                                gold_score = 10
+                            else:
+                                gold_score = 1
+                        elif src_role in [
+                            "Minion",
+                            "Assassin",
+                        ] and tgt_role in [
+                            "Minion",
+                            "Assassin",
+                            "Merlin",
+                        ]:
+                            if to_guess_role in ["Minion", "Assassin"]:
+                                gold_score = 10
+                            else:
+                                gold_score = 1
+                        else:
+                            guesses = []
+                            for role_guess in row["role_guess"][round_i][
+                                tgt_player
+                            ]:
+                                guesses.append(role_guess["tgt_player"])
+                                if role_guess["tgt_player"] == int(
+                                    src_player_i
+                                ):
+                                    gold_score: int = role_guess["output"][
+                                        "score"
+                                    ]
+                                    break
+                            else:
+                                raise ValueError(
+                                    f"`gold_score` not provided in role_guess. Player {tgt_player} only guesses Player {guesses}'s role, but not {src_player_i}."
+                                )
+
+                        sign: bool = (
+                            belief_dict["tgt_role"]
+                            == roles[belief_dict["tgt_player"]]
+                        )
+                        out_data.append(
+                            (
+                                belief_dict["prompt"],
+                                json.dumps(
+                                    belief_dict["output"],
+                                    indent=4,
+                                    ensure_ascii=False,
+                                ),
+                                ROLE_BELIEF_SCORE[
+                                    abs(
+                                        belief_dict["output"]["score"]
+                                        - gold_score
+                                    )
+                                ],
+                            )
+                        )
+            # summaries
+            for sum_round in row["summaries"]:
+                for player_i, sum_dict in sum_round.items():
+                    # ["prompt", "resp"]
+                    out_data.append(
+                        (
+                            sum_dict["prompt"],
+                            sum_dict["resp"],
+                            player_rewards[int(player_i)],
+                        )
+                    )
+
+            # assassin
+            if row["assassin"]:
+                out_data.append(
+                    (
+                        row["assassin"]["prompt"],
+                        json.dumps(
+                            {
+                                "rationale": row["assassin"]["rationale"],
+                                "merlin": row["assassin"]["merlin"],
+                            },
+                            indent=4,
+                            ensure_ascii=False,
+                        ),
+                        TRAJ_LOSE_REWARD if good_win else TRAJ_WIN_REWARD,
+                    )
+                )
+        prompt_tensors = []
+        response_tensors = []
+        reward_tensors = []
+        for prompt, response, reward in out_data:
+            template = get_conv_template("llama-3")
+            template.append_message(template.roles[0], prompt)
+            template.append_message(template.roles[1], None)
+            prompt = template.get_prompt()
+            prompt_tensor = tokenizer(prompt, return_tensors="pt")[
+                "input_ids"
+            ][0]
+            template.update_last_message(response)
+            response = template.get_prompt()
+            response_tensor = tokenizer(response, return_tensors="pt")[
+                "input_ids"
+            ][0]
+            response_tensor = response_tensor[prompt_tensor.shape[0] :]
+            reward_tensor = torch.tensor(reward, dtype=torch.float64)
+
+            prompt_tensors.append(prompt_tensor)
+            response_tensors.append(response_tensor)
+            reward_tensors.append(reward_tensor)
+
+        if n_samples < 0:
+            global_bz = mini_batch_size * grad_accum
+            n_samples = (len(prompt_tensors) // global_bz) * global_bz
+        self.prompt_tensors = prompt_tensors[:n_samples]
+        self.response_tensors = response_tensors[:n_samples]
+        self.reward_tensors = reward_tensors[:n_samples]
+
+    def __getitem__(self, index) -> Any:
+        return (
+            self.prompt_tensors[index],
+            self.response_tensors[index],
+            self.reward_tensors[index],
+        )
+
+    def __len__(self) -> int:
+        return len(self.prompt_tensors)
 
 
 def convert_int_keys(d):
@@ -69,7 +343,7 @@ def get_model(
         )
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        # device_map="auto",
+        device_map=None if is_deepspeed_used else "auto",
         # device_map={"": Accelerator().local_process_index},
         torch_dtype=torch.float16,
         # torch_dtype=torch.float32,
@@ -91,9 +365,13 @@ def get_model(
         )
         model = get_peft_model(model, lora_config)
 
-    model = AutoModelForCausalLMWithValueHead.from_pretrained(model)
-    model_ref = create_reference_model(model)
-    return model, model_ref
+    rl_model = AutoModelForCausalLMWithValueHead.from_pretrained(model)
+    return rl_model, None
+    rl_model_ref = AutoModelForCausalLMWithValueHead.from_pretrained(
+        model
+    ).eval()
+    # model_ref = create_reference_model(model)
+    return rl_model, rl_model_ref
 
 
 def get_tokenizer(model_name: str):
@@ -388,40 +666,15 @@ def main(
         model_name, lora_path=lora_path, lora_config=lora_config
     )
     tokenizer = get_tokenizer(model_name)
-
-    dataset = get_data(
+    dataset = PPODataset(
         filename,
+        mini_batch_size=mini_batch_size,
+        grad_accum=grad_accum,
+        n_samples=n_samples,
+        tokenizer=tokenizer,
         include_guess_role=include_guess_role,
         include_guess_belief=include_guess_belief,
     )
-
-    prompt_tensors = []
-    response_tensors = []
-    reward_tensors = []
-    for prompt, response, reward in dataset:
-        template = get_conv_template("llama-3")
-        template.append_message(template.roles[0], prompt)
-        template.append_message(template.roles[1], None)
-        prompt = template.get_prompt()
-        prompt_tensor = tokenizer(prompt, return_tensors="pt")["input_ids"][0]
-        template.update_last_message(response)
-        response = template.get_prompt()
-        response_tensor = tokenizer(response, return_tensors="pt")[
-            "input_ids"
-        ][0]
-        response_tensor = response_tensor[prompt_tensor.shape[0] :]
-        reward_tensor = torch.tensor(reward, dtype=torch.float64)
-
-        prompt_tensors.append(prompt_tensor)
-        response_tensors.append(response_tensor)
-        reward_tensors.append(reward_tensor)
-
-    if n_samples < 0:
-        global_bz = mini_batch_size * grad_accum
-        n_samples = (len(prompt_tensors) // global_bz) * global_bz
-    prompt_tensors = prompt_tensors[:n_samples]
-    response_tensors = response_tensors[:n_samples]
-    reward_tensors = reward_tensors[:n_samples]
 
     # create a ppo trainer
     trainable_params, all_param = count_parameters(model)
@@ -432,18 +685,18 @@ def main(
     )
     # initialize trainer
     ppo_config = PPOConfig(
-        batch_size=len(prompt_tensors),
+        batch_size=len(dataset),
         mini_batch_size=mini_batch_size,
         gradient_accumulation_steps=grad_accum,
         ppo_epochs=n_epochs,
         log_with="wandb",
         task_name="RoleGuessPPO",
     )
+    Accelerator().state.deepspeed_plugin.deepspeed_config[
+        "train_micro_batch_size_per_gpu"
+    ] = mini_batch_size
     ppo_trainer = PPOTrainer(
-        ppo_config,
-        model,
-        model_ref,
-        tokenizer,
+        ppo_config, model, model_ref, tokenizer, dataset=dataset
     )
 
     # train model for one step with ppo
@@ -456,7 +709,9 @@ def main(
     # response_masks (List[`torch.FloatTensor`], *optional*)):
     #     List of tensors containing masks of the response tokens.
     train_stats = ppo_trainer.step(
-        prompt_tensors, response_tensors, reward_tensors
+        dataset.prompt_tensors,
+        dataset.response_tensors,
+        dataset.reward_tensors,
     )
     ppo_trainer.save_pretrained(save_path)
     print(f"Trained model saved to {save_path}.")
